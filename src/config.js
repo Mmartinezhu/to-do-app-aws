bd94056db3583cbfe193e963d725cc7d795a8c8d
Manuel santiago martinez hurtado <159917835+Mmartinezhu@users.noreply.github.com>
2026-05-20 16:26:34 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,9 @@
 export const cognitoConfig = {
-  UserPoolId: "us-east-1_g5qyxjQgv",
-  ClientId: "1l1koh0smbgre51r12afop08l8"
-};
+  UserPoolId: process.env.REACT_APP_COGNITO_USER_POOL_ID,
+  ClientId: process.env.REACT_APP_COGNITO_CLIENT_ID,
+}
+
+export const apiConfig = {
+  baseUrl: process.env.REACT_APP_API_BASE_URL,
+  apiKey: process.env.REACT_APP_API_KEY,
+}
